acc698a95655a9bc0dc8fe2c7c07379c2befe773
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-06-07 08:43:38 +0600

    testing button

--- src/components/AButton/AButton.test.ts
+++ src/components/AButton/AButton.test.ts
@@ -3,7 +3,7 @@ import AButton from './AButton.vue'
 
 describe('AButton.vue', () => {
   it('Should render', async () => {
-    const wrapper = await mount(AButton, {
+    const wrapper = await mount(AButton as any, {
       props: {
         raised: true,
         outlined: true,
@@ -14,31 +14,45 @@ describe('AButton.vue', () => {
         loading: true,
         block: true,
         variant: true,
-        size: true
+        size: true,
+        disabled: false
       },
       slots: {
         default: 'I am btn'
       }
     })
-    console.log('classes:', wrapper.classes())
+    console.log('classed', wrapper.classes())
 
-    // expect(wrapper.find('button').exists())
-    // expect(wrapper.text()).toContain('I am btn')
-    // expect(wrapper.classes().includes('a-raised'))
-    // expect(wrapper.classes().includes('a-outlined'))
-    // expect(wrapper.classes().includes('a-rounded'))
-    // expect(wrapper.classes().includes('a-flat'))
-    // expect(wrapper.classes().includes('a-text'))
-    // expect(wrapper.classes().includes('a-icon'))
-    // expect(wrapper.classes().includes('a-loading'))
-    // expect(wrapper.classes().includes('a-block'))
-    expect(false)
+    expect(wrapper.find('button').exists())
+    expect(wrapper.text()).toContain('I am btn')
+    // expect(wrapper.classes()).includes('a-raised')
+    // expect(wrapper.classes()).includes('a-outlined')
+    // expect(wrapper.classes()).includes('a-rounded')
+    // expect(wrapper.classes()).includes('a-flat')
+    // expect(wrapper.classes()).includes('a-text')
+    // expect(wrapper.classes()).includes('a-icon')
+    // expect(wrapper.classes()).includes('a-loading')
+    // expect(wrapper.classes()).includes('a-block')
+    // expect(wrapper.classes()).includes('a-primary')
+    expect(wrapper.attributes().disabled).toBeUndefined()
   })
+
   it('Should be interactive', async () => {
-    const wrapper = mount(AButton, {})
-    await wrapper.setProps({
-      variant: 'info'
+    const wrapper = mount(AButton as any, {
+      props: {
+        disabled: true
+      }
     })
-    // expect(wrapper.classes()).includes('a-info')
+
+    expect(wrapper.attributes()).haveOwnProperty('disabled')
+
+    await wrapper.setProps({
+      variant: 'info',
+      disabled: false
+    })
+
+    expect(wrapper.classes()).includes('a-info')
+
+    expect(wrapper.attributes().disabled).toBeUndefined()
   })
 })
--- src/components/AButton/AButton.vue
+++ src/components/AButton/AButton.vue
@@ -1,7 +1,21 @@
 <script lang="ts" setup>
-defineProps({})
+import { ref, reactive, computed } from 'vue'
+
+const props = defineProps({
+  variant: {
+    type: String,
+    default: 'primary'
+  }
+})
 defineEmits([])
+
+const classes = computed(() => {
+  return {
+    'a-button': true,
+    [`a-${props.variant}`]: true
+  }
+})
 </script>
 <template>
-  <button><slot /></button>
+  <button :class="classes"><slot /></button>
 </template>
