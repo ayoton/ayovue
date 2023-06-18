751725e0fdb2ec308111917792e656a7827ae3b9
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-06-18 13:15:15 +0600

    dialog added

--- src/components/ADialog/ADialog.test.ts
+++ src/components/ADialog/ADialog.test.ts
@@ -1,14 +1,14 @@
-import { mount } from "@vue/test-utils";
-import ADialog from "./ADialog.vue";
+import { mount } from '@vue/test-utils'
+import ADialog from './ADialog.vue'
 
-describe("ADialog.vue", () => {
-  it("Should render", () => {
-    const wrapper = mount(ADialog, {});
-    expect(wrapper.text()).toContain("");
-  });
-  it("Should be interactive", async () => {
-    const wrapper = mount(ADialog, {});
-    await wrapper.setProps({});
-    expect(wrapper.text()).toContain("");
-  });
-});
+describe('ADialog.vue', () => {
+  it('Should render', () => {
+    const wrapper = mount(ADialog as any, {})
+    expect(wrapper.text()).toContain('')
+  })
+  it('Should be interactive', async () => {
+    const wrapper = mount(ADialog as any, {})
+    await wrapper.setProps({})
+    expect(wrapper.text()).toContain('')
+  })
+})
