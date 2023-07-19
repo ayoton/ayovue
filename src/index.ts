be24ccb1e5f4dba072b832758625825499b525c2
Hasinoor Rahman <hasinoorit@gmail.com>
2023-07-19 12:16:36 +0600

    Select component created

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import AButton from './components/AButton/index'
-import AButtonGroup from './components/AButtonGroup/index'
-import ACheckbox from './components/ACheckbox/index'
-import ADialog from './components/ADialog/index'
-import AInput from './components/AInput/index'
-import AInputFile from './components/AInputFile/index'
-import ARadio from './components/ARadio/index'
-import ASwitch from './components/ASwitch/index'
-import AToasts from './components/AToasts/index'
-export { useToast } from './composables/useToast/index'
-export { vRipple } from './directives/vRipple'
-export { vTooltip } from './directives/vTooltip'
-export { AButton, AButtonGroup, ACheckbox, ADialog, AInput, AInputFile, ARadio, AToasts, ASwitch }
+import AButton from "./components/AButton/index";
+import AButtonGroup from "./components/AButtonGroup/index";
+import ACheckbox from "./components/ACheckbox/index";
+import ADialog from "./components/ADialog/index";
+import AInput from "./components/AInput/index";
+import AInputFile from "./components/AInputFile/index";
+import ARadio from "./components/ARadio/index";
+import ASelect from "./components/ASelect/index";
+import ASwitch from "./components/ASwitch/index";
+import AToasts from "./components/AToasts/index";
+export { useToast } from "./composables/useToast/index";
+export { vRipple } from "./directives/vRipple";
+export { vTooltip } from "./directives/vTooltip";
+export { AButton, AButtonGroup, ACheckbox, ADialog, AInput, AInputFile, ARadio, ASelect, ASwitch, AToasts };
