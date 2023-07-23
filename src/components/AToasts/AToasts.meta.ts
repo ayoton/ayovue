94f25c1c3249d4621e3c65dd9afe4e017d2ef64e
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-07-23 10:45:17 +0600

    atoasts meta added

--- src/components/AToasts/AToasts.meta.ts
+++ src/components/AToasts/AToasts.meta.ts
@@ -1,5 +1,20 @@
-import type { AComponentMeta } from "../../utils/meta-types";
+import type { AComponentMeta } from '../../utils/meta-types'
 
 export const AToastsMeta: AComponentMeta = {
-  name: "AToasts",
-};
+  name: 'AToasts',
+  description: 'Toast is used to display messages in an overlay.',
+
+  props: {
+    position: {
+      type: String,
+      description: 'To specify the position',
+      default: ''
+    },
+
+    group: {
+      type: String,
+      description: 'To group the toast',
+      default: 'default'
+    }
+  }
+}
