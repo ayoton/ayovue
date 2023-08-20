2f506823916a9bbfb46d5fab92e177aae231d3f9
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-08-20 11:17:56 +0600

    build script updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { Plugin } from 'vue'
 import AButton from "./components/AButton/index";
 import AButtonGroup from "./components/AButtonGroup/index";
 import ACheckbox from "./components/ACheckbox/index";
@@ -14,3 +15,37 @@ export { useToast } from "./composables/useToast/index";
 export { vRipple } from "./directives/vRipple";
 export { vTooltip } from "./directives/vTooltip";
 export { AButton, AButtonGroup, ACheckbox, ACollapse, ADialog, AInput, AInputFile, ARadio, ASelect, ASpinner, ASwitch, AToasts };
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+      AButton: typeof AButton,
+      AButtonGroup: typeof AButtonGroup,
+      ACheckbox: typeof ACheckbox,
+      ACollapse: typeof ACollapse,
+      ADialog: typeof ADialog,
+      AInput: typeof AInput,
+      AInputFile: typeof AInputFile,
+      ARadio: typeof ARadio,
+      ASelect: typeof ASelect,
+      ASpinner: typeof ASpinner,
+      ASwitch: typeof ASwitch,
+      AToasts: typeof AToasts
+  }
+}
+const AyoVue: Plugin = {
+  install(app) {
+    app.component('AButton', AButton)
+    app.component('AButtonGroup', AButtonGroup)
+    app.component('ACheckbox', ACheckbox)
+    app.component('ACollapse', ACollapse)
+    app.component('ADialog', ADialog)
+    app.component('AInput', AInput)
+    app.component('AInputFile', AInputFile)
+    app.component('ARadio', ARadio)
+    app.component('ASelect', ASelect)
+    app.component('ASpinner', ASpinner)
+    app.component('ASwitch', ASwitch)
+    app.component('AToasts', AToasts)
+  }
+}
+export default AyoVue
+  
